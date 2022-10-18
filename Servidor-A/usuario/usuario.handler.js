adcbb4aacf1086e3a2e79846d63ea2e558a6780c
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2022-10-17 21:42:45 -0300

    Docker S-A e S-B com firebase

--- Servidor-A/usuario/usuario.handler.js
+++ Servidor-A/usuario/usuario.handler.js
@@ -1,48 +1,49 @@
-const list = [];
+const crud = require("../Crud");
+const nomeTabela = "Users";
 
-async function adicionarUsuario(usuario) {
-    if (usuario.login != undefined) {
-        if (usuario.cpf != undefined) {
-            if (usuario.senha != undefined) {
-                if (usuario.telefone != undefined) {
-                    list.push(usuario);
-                    return usuario;
-                } else {
-                    return "É necessário informar o telefone!"
-                }
-            } else {
-                return "É necessário informar a senha!"
-            }
-        } else {
-            return "É necessário informar o cpf!"
-        }
-    } else {
-        return "É necessário informar o nome!"
+async function adicionarUsuario(dados = {CPF: "", Name: "", Password:""}) {
+    if (!dados.CPF){
+        return {error: "0001", 
+        message: "É necessário preencher os parametros da requisição!", 
+        camposNecessarios: ["CPF"]}
     }
+    if(!dados.Name){
+        return {error: "0002", 
+        message: "É necessário preencher os parametros da requisição!", 
+        camposNecessarios: ["name"]}
+    }   
+    if (!dados.Password){
+        return {error: "0003", 
+        message: "É necessário preencher os parametros da requisição!", 
+        camposNecessarios: ["password"]}
+    }
+    const user = await crud.save(nomeTabela, undefined, dados);
+    return user;
 }
 
 async function login(usuario) {
     console.log("chegou", usuario)
-    if (usuario.login != undefined) {
-        if (usuario.senha != undefined) {
-            for(let i = 0; i < list.length; i++) {
-                if(list[i].login == usuario.login && list[i].senha == usuario.senha){
-                    return 1;
-                }
-            }
-            return -1;
+    if (usuario.userCPF != undefined && usuario.password != undefined) {
+        console.log("Usu", usuario);
+        const resultado = await crud.getWithFilter("Users", "CPF", "==", usuario.userCPF);
+        console.log(resultado);
+        console.log("usuariopas", usuario.password);
+        console.log("resultpas", resultado.Password);
+        if(resultado[0].Password == usuario.password){
+            console.log("Entrou")
+            return 1;
         } else {
-            return 2;
+            return -1;
         }
     } else {
         console.log("ok", usuario.login)
-        return 3;
+        return 2;
     }
-
 }
 
 async function mostrarUsuario() {
-    return list;
+    const mostrar = await crud.get(nomeTabela);
+    return mostrar;
 }
 
 module.exports = {
--- Servidor-B/produtos/produtos.handler.js
+++ Servidor-B/produtos/produtos.handler.js
@@ -1,55 +1,70 @@
-const list = [];
+const crud = require("../Crud");
+const nomeTabela = "Products";
 
-async function adicionarProduto(produto = {login: "", senha: "", nome: "", descricao: "", preco: ""}) {
-    if (produto.login != undefined) {
-        if (produto.senha != undefined) {
-            if (produto.nome != undefined) {
-                if (produto.descricao != undefined) {
-                    if (produto.preco != undefined) {
-                    
-                        const body = {login: produto.login, senha: produto.senha};
-                        
-                        const fetch = require('node-fetch');
-                        const response = await fetch('http://localhost:3000/api/usuario/login', { 
-                            headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json'
-                              },
-                            method: 'POST', 
-                            body: JSON.stringify(body) 
-                        })
-
-                        const resposta = await response.json();
-                        console.log("Aqu 4", resposta);
-
-                        if(resposta == -1){
-                            delete produto.login;
-                            delete produto.senha;
-                            list.push(produto);
-                            return produto;
-                        } else {
-                            return "Usuário inexistente, produto não cadastrado!"
-                        }
-                    } else {
-                        return "É necessário informar o preco!"
-                    }
-                } else {
-                    return "É necessário informar a descricao!"
-                }
-            } else {
-                return "É necessário informar o nome!"
-            }
-        } else {
-            return "É necessário informar a senha!"
+async function adicionarProduto(produto = { userCPF: "", password: "", name: "", description: "", price: "" }) {
+    if (!produto.userCPF) {
+        return {
+            error: "0001",
+            message: "É necessário preencher os parametros da requisição!",
+            camposNecessarios: ["userCPF"]
         }
+    }
+    if (!produto.password) {
+        return {
+            error: "0002",
+            message: "É necessário preencher os parametros da requisição!",
+            camposNecessarios: ["password"]
+        }
+    }
+    if (!produto.name) {
+        return {
+            error: "0003",
+            message: "É necessário preencher os parametros da requisição!",
+            camposNecessarios: ["name"]
+        }
+    }
+    if (!produto.description) {
+        return {
+            error: "0004",
+            message: "É necessário preencher os parametros da requisição!",
+            camposNecessarios: ["description"]
+        }
+    }
+    if (!produto.price) {
+        return {
+            error: "0005",
+            message: "É necessário preencher os parametros da requisição!",
+            camposNecessarios: ["price"]
+        }
+    }
+
+    const body = { userCPF: produto.userCPF, password: produto.password };
+    const fetch = require('node-fetch');
+    const response = await fetch('http://destino:3000/api/usuario/login', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+
+    const resposta = await response.json();
+    console.log("Aqu 4", resposta);
+    delete produto.password
+    if (resposta == 1) {
+        const dado = await crud.save(nomeTabela, undefined, produto);
+        return dado;
+    } else if (resposta == -1) {
+        return "Usuário e/ou Senha Incorretos!"
     } else {
-        return "É necessário informar o login!"
+        return "É necessário preencher os parâmetros da requisição"
     }
 }
 
-
 async function mostrarProduto() {
-    return list;
+    const mostrar = await crud.get(nomeTabela);
+    return mostrar;
 }
 
 module.exports = {
